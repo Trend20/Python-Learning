fb7756e3b0cd5e8ff8758e72f9378ee6d41db75f
Trend20 <enockomondi305@gmail.com>
2023-02-13 16:37:31 +0300

    first program

---
+++ main.py
@@ -0,0 +1,21 @@
+print('Hello World!')
+
+# print a string with a calculation
+print(f'20 days are {20 * 24 * 60} minutes ');
+
+print('This is my first Python Program!');
+
+# function to print my name
+def printName():
+    print('My name is Enock Omondi Otuoma');
+printName();
+
+# function to list down all the student marks
+def listStudentMarks():
+    print('The first student scored ' + str(30) + ' marks');
+    print('The second student scored ' + str(23) + ' marks');
+    print('The third student scored ' + str(15) + ' marks');
+    print('The forth student scored ' + str(10) + ' marks');
+    print('The fifth student scored ' + str(7) + ' marks');
+    print('The last student scored ' + str(5) + ' marks');
+listStudentMarks();
